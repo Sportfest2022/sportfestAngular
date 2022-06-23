3e5015af98880e80074b4e513c69847fc1d43908
JAB1305 <54218874+JAB1305@users.noreply.github.com>
2022-06-23 09:51:10 +0200

    Add success screen

--- src/app/util/routing/routing.component.ts
+++ src/app/util/routing/routing.component.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from "../../login/login.component";
+import {LoginComponent} from "../login/login.component";
 import {AuthguardService} from "../../services/authguard.service";
 import {Type1formComponent} from "../../game/type1form/type1form.component";
 import {DefaultComponent} from "../default/default.component";
 import {Type2formComponent} from "../../game/type2form/type2form.component";
+import {SuccessComponent} from "../success/success.component";
 
 const routes: Routes = [
-  { path: 'type1', component: Type2formComponent, canActivate: [AuthguardService]},
+  { path: 'type1', component: Type1formComponent, canActivate: [AuthguardService]},
+  { path: 'type2', component: Type2formComponent, canActivate: [AuthguardService]},
   { path: 'login', component: LoginComponent},
+  { path: 'success', component: SuccessComponent},
   { path: '**', component: DefaultComponent}
 ];
 
